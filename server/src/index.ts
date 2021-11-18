4df662b6a83fd85de4d974a063964bf28f4307bd
Tomer Levy <tomer.levy@tipalti.com>
2021-11-18 21:07:33 +0200

    add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,9 +40,19 @@ async function startServer() {
   
   
   const app = express();
+  const allowedDomains = ['http://localhost:3000', 'https://ecoocean-server-73kh3tgoha-ew.a.run.app']
   const options = {
-    origin: 'http://localhost:3000',
-  }
+        origin: function (origin, callback) {
+          // bypass the requests with no origin (like curl requests, mobile apps, etc )
+          if (!origin) return callback(null, true);
+
+          if (allowedDomains.indexOf(origin) === -1) {
+            var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+            return callback(new Error(msg), false);
+          }
+          return callback(null, true);
+        },
+      }
   ;
 
   app.use(cors(options));
