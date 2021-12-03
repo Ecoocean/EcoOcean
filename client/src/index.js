32aac6df822ee2fc0493fcf5f8112171e3d8a2ff
Tomer Levy <tomer.levy@tipalti.com>
2021-12-03 08:54:26 +0200

    another try

--- client/src/index.js
+++ client/src/index.js
@@ -27,10 +27,17 @@ const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
     const token = localStorage.getItem('token');
     // return the headers to the context so httpLink can read them
+    if(token){
+        return {
+            headers: {
+                ...headers,
+                authorization: `Bearer ${token}`,
+            }
+        }
+    }
     return {
         headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : "Bearer",
+            ...headers
         }
     }
 });
