a2045053abcc76eb16f82c29f8e54cee98394efc
Tomer Levy <tomer1@post.bgu.ac.il>
2021-12-11 16:51:20 +0200

    change index

--- client/src/index.js
+++ client/src/index.js
@@ -14,82 +14,93 @@ import {onError} from "@apollo/client/link/error";
 import {setSnackBar} from "./SnackBarUtils";
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 
+function createLink() {
+    const link = createUploadLink({
+        uri: process.env.REACT_APP_SERVER_ENDPOINT_URL,
+    });
 
-const link = createUploadLink({
-    uri: process.env.REACT_APP_SERVER_ENDPOINT_URL,
-});
+    const graphqlEndpoint = `${process.env.REACT_APP_WEB_SOCKET_TYPE}://${process.env.REACT_APP_SERVER_ENDPOINT}`;
 
-const graphqlEndpoint = `${process.env.REACT_APP_WEB_SOCKET_TYPE}://${process.env.REACT_APP_SERVER_ENDPOINT}`;
+    const wsClient = new SubscriptionClient(
+        graphqlEndpoint,
+        {
+          reconnect: true,
+          lazy: true,
+          connectionParams: (() => {
+              return localStorage.getItem('token')
+              ? {
+                  authorization: `Bearer ${localStorage.getItem('token')}`,
+              }
+              : {}}),
+        }
+    );
 
-const wsClient = new SubscriptionClient(
-    graphqlEndpoint,
-    {
-      reconnect: true,
-      connectionParams: localStorage.getItem('token')
-            ? {
-                authorization: `Bearer ${localStorage.getItem('token')}`,
+    wsClient.onConnected(() => console.log('CONNECTED to GraphqlEndpoint'));
+    wsClient.onDisconnected(() => setSnackBar('No internet connection - offline mode', 'error'));
+    wsClient.onReconnected(() => setSnackBar('Reconnected to the server', 'success'));
+
+    const wsLink = new WebSocketLink(wsClient);
+    // const wsLink = new WebSocketLink({
+    //     uri: `${process.env.REACT_APP_WEB_SOCKET_TYPE}://${process.env.REACT_APP_SERVER_ENDPOINT}`,
+    //     options: {
+           
+    //     },
+    // });
+
+    const authLink = setContext((_, { headers }) => {
+        // get the authentication token from local storage if it exists
+        const token = localStorage.getItem('token');
+        // return the headers to the context so httpLink can read them
+        if(token){
+            return {
+                headers: {
+                    ...headers,
+                    authorization: `Bearer ${token}`,
+                }
             }
-            : {},
-    }
-);
-
-wsClient.onConnected(() => console.log('CONNECTED to GraphqlEndpoint'));
-wsClient.onDisconnected(() => setSnackBar('No internet connection - offline mode', 'error'));
-wsClient.onReconnected(() => setSnackBar('Reconnected to the server', 'success'));
-
-const wsLink = new WebSocketLink(wsClient);
-
-const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem('token');
-    // return the headers to the context so httpLink can read them
-    if(token){
+        }
         return {
             headers: {
-                ...headers,
-                authorization: `Bearer ${token}`,
+                ...headers
             }
         }
-    }
-    return {
-        headers: {
-            ...headers
+    });
+
+    // Log any GraphQL errors or network error that occurred
+    const errorLink = onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+            graphQLErrors.forEach(({message, locations, path}) => {
+                console.log(
+                    `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+                );
+                setSnackBar(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`, 'error');
+            })
         }
-    }
-});
+        if (networkError) {
+            console.log(`[Network error]: ${networkError}`);
+            setSnackBar(`[Network error]: could not establish connection to the server`, 'error')
+        }
+    });
 
-// Log any GraphQL errors or network error that occurred
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors) {
-        graphQLErrors.forEach(({message, locations, path}) => {
-            console.log(
-                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+    const splitLink = split(
+        ({ query }) => {
+            const definition = getMainDefinition(query);
+            return (
+                definition.kind === "OperationDefinition" &&
+                definition.operation === "subscription"
             );
-            setSnackBar(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`, 'error');
-        })
-    }
-    if (networkError) {
-        console.log(`[Network error]: ${networkError}`);
-        setSnackBar(`[Network error]: could not establish connection to the server`, 'error')
-    }
-});
+        },
+        wsLink,
+        link
+    );
 
-const splitLink = split(
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === "OperationDefinition" &&
-            definition.operation === "subscription"
-        );
-    },
-    wsLink,
-    link
-);
+    return errorLink.concat(authLink.concat(splitLink));
+}
 
 const client = new ApolloClient({
     ssrMode: typeof window === "undefined",
     cache: cache,
-    link: errorLink.concat(authLink.concat(splitLink)),
+    link: createLink(),
     defaultOptions: {
         query: {
             errorPolicy: 'all',
@@ -99,6 +110,10 @@ const client = new ApolloClient({
         },
     }
 });
+
+client.onResetStore(async () => {
+    client.setLink(createLink());
+})
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
