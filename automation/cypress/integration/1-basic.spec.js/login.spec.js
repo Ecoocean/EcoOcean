388c9fed36e6ed115cc03dc877c9fc49c884d8f2
Tomer Levy <tomer.levy@tipalti.com>
2022-03-19 20:46:55 +0100

    remove wait

--- automation/cypress/integration/1-basic.spec.js/login.spec.js
+++ automation/cypress/integration/1-basic.spec.js/login.spec.js
@@ -9,12 +9,10 @@ describe('login to ecoocean', () => {
   it('login as admin to ecoocean', () => {
     cy.percySnapshot('login screen');
     cy.login('admin@gmail.com', '123456');
-    cy.wait(5000);
     cy.get('div[class="sidebar-pane active"]', { timeout: 10000 }).should('be.visible');
     cy.percySnapshot('home page - side panel open');
     cy.get('div[class="sidebar-close"]').click({multiple: true, force: true});
     cy.get('div[class="sidebar-pane active"]').should('not.exist');
-    cy.wait(5000);
     cy.percySnapshot('home page - side panel closed');
   })
 
