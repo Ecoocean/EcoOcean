621c61ce5a72805f27acf4ac4ea8eead2b24bd87
Tomer Levy <tomer.levy@tipalti.com>
2022-03-25 18:21:49 +0300

    test again

---
+++ automation/cypress/support/waitForServices.js
@@ -0,0 +1,19 @@
+const waitOn = require('wait-on');
+
+const opts = {
+    resources: [
+        'http://localhost:3000',
+    ],
+    log: true,
+    timeout: 120000
+}
+
+(async () =>{
+    try {
+        await waitOn(opts);
+        // once here, all resources are available
+    } catch (err) {
+        console.log('failed to wait for services');
+        throw new Error('failed to wait for services')
+    }
+})()
