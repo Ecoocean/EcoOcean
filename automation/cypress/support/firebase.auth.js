12bce1218da987ac596f1616c43261c3df379310
Tomer Levy <tomer.levy@tipalti.com>
2022-03-25 18:51:08 +0300

    cypress without service key

--- automation/cypress/support/firebase.auth.js
+++ automation/cypress/support/firebase.auth.js
@@ -1,12 +1,9 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../serviceAccount.json");
 
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL:
-        "https://ecoocean-default-rtdb.europe-west1.firebasedatabase.app",
-    storageBucket: "gs://ecoocean.appspot.com",
+    projectId: 'ecoocean',
+    credential: admin.credential.applicationDefault()
 });
 
 module.exports = admin.auth();
