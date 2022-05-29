a0705ebc9d7e60ea5085e48829107285c4804d22
Tomer Levy <tomer.levy@tipalti.com>
2022-05-29 20:11:17 +0300

    let see

--- server/src/firestore.ts
+++ server/src/firestore.ts
@@ -6,28 +6,28 @@ const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 const dbHost = process.env.DB_HOST;
 const dbPort = process.env.DB_PORT;
+const serviceKeyBase = process.env.GCP_SERVICE_KEY_BASE
 
-(async () => {
-  if (process.env.ENVIRONMENT === 'prod') {
-    const response = await fetch('./key.json');
-    console.log(response);
-    const json = await response.json();
-    console.log(`google config ${json}`)
-    admin.initializeApp({
-      credential: admin.credential.cert(json),
-      databaseURL:
-          "https://ecoocean-default-rtdb.europe-west1.firebasedatabase.app",
-      storageBucket: "gs://ecoocean.appspot.com",
-    });
-  } else {
-    admin.initializeApp({
-      projectId: "ecoocean",
-      databaseURL:
-          "https://ecoocean-default-rtdb.europe-west1.firebasedatabase.app",
-      storageBucket: "http:localhost:9199",
-    });
-  }
-})();
+if (process.env.ENVIRONMENT === 'prod') {
+  const buff = new Buffer(serviceKeyBase, 'base64');
+  const text = buff.toString();
+  const json = JSON.parse(text);
+  console.log(`google config ${json}`)
+  admin.initializeApp({
+    credential: admin.credential.cert(json),
+    databaseURL:
+        "https://ecoocean-default-rtdb.europe-west1.firebasedatabase.app",
+    storageBucket: "gs://ecoocean.appspot.com",
+  });
+} else {
+  admin.initializeApp({
+    projectId: "ecoocean",
+    databaseURL:
+        "https://ecoocean-default-rtdb.europe-west1.firebasedatabase.app",
+    storageBucket: "http:localhost:9199",
+  });
+}
+
 
 
 export const auth = admin.auth();
